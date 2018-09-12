14ffa895b3de65ceffd99d5aefac7b32344c9b44
1nfer <37373380+1nfer@users.noreply.github.com>
2018-09-12 11:30:29 -0500

    Update main.cpp

--- bits/main.cpp
+++ bits/main.cpp
@@ -5,51 +5,92 @@ using namespace std;
 
 template  <std::size_t N>
 bitset<2*N> multi(bitset<N> m, bitset<N> q){
-  bitset<N>a(0); //acumulador
-  int c=0;//acarreo
-  bitset<N>qr(q);
-  bitset<2*N>res(0); //resultado
-  int cont=N;
-  int j=0;
+    bitset<N>a(0);
+    int c;
+    bitset<2*N>res(0);
+    int cont=N;
 
-  while(cont!=0){
-    if(q[j]==1){
-      bitset<N> suma;
-      for(int i=0; i<N; i++){
-        suma[i]=((a[i] ^ m[i]) ^ c);
-        c=((a[i] & m[i]) | ((a[i] ^ m[i]) & c));
-      }
-      a=suma;
-    }
+    while(cont!=0){
+        if(q[0]==1){
+            bitset<N> suma;
+            for(int i=0; i<N; i++){
+                suma[i]=((a[i] ^ m[i]) ^ c);
+                c=((a[i] & m[i]) | ((a[i] ^ m[i]) & c));
+            }
+            a=suma;
+        }
     int tmp=a[0];
+
     a>>=1;
-    qr>>=1;
-    qr[N-1]=tmp;
+    q>>=1;
+    q[N-1]=tmp;
     a[N-1]=c;
+
     c=0;
     cont--;
-    j++;
-  }
+    }
 
-  for(int i=0;i<2*N;i++){
-    if(i<N)
-      res[i]=qr[i];
-    else
-      res[i]=a[i-N];
-  }
-  return res;
+    for(int i=0;i<2*N;i++){
+        if(i<N)
+            res[i]=q[i];
+        else
+            res[i]=a[i-N];
+    }
+    return res;
+}
+
+template  <std::size_t N>
+bitset<2*N> booth(bitset<N> m, bitset<N> q){
+    bitset<N>a(0);
+    int q1=q[0];
+    bitset<2*N> res(0);
+    int cont=N;
+
+    while(cont!=0){
+        if(q==1 && q1==0){
+            int c=0;
+            bitset<4> resta;
+            bitset<4> suma;
+            bitset<4> u(1);
+            m1=~m;
+
+            for(int i=0; i<4; i++){
+                suma[i]=((m1[i] ^ u[i]) ^ c);
+                c=((m1[i] & u[i]) | ((m1[i] ^ u[i]) & c));
+            }
+
+            bitset<4>res=suma;
+            for(int i=0; i<4; i++){
+                resta[i]=((suma[i] ^ a[i]) ^ c);
+                c=((suma[i] & a[i]) | ((suma[i] ^ a[i]) & c));
+            }
+            suma=resta;
+
+        }
+        else if(q==0 && q1==1){
+            bitset<N> suma;
+            int c=0;
+            for(int i=0; i<N; i++){
+                suma[i]=((a[i] ^ m[i]) ^ c);
+                c=((a[i] & m[i]) | ((a[i] ^ m[i]) & c));
+            }
+            a=suma;
+        }
+    }
+
 }
 
 
 int main(){
-  bitset<4> mndo;
-  bitset<4> mdor;
-  cout<<"multiplicando: ";
-  cin>>mndo;
-  cout<<"multiplicador: ";
-  cin>>mdor;
+    bitset<4> mndo;
+    bitset<4> mdor;
+    cout<<"multiplicando: ";
+    cin>>mndo;
+    cout<<"multiplicador: ";
+    cin>>mdor;
 
-  cout<<multi(mndo,mdor);
+  //cout<<multi(mndo,mdor);
+  cout<<suma;
 
   return 0;
 }
